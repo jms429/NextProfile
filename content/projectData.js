c96ca7f4cc927d377fd5297e4879db33a4597fd8
jms429 <jms429@gmail.com>
2022-11-08 12:50:48 -0500

    added projects page

---
+++ content/projectData.js
@@ -0,0 +1,15 @@
+module.exports = [
+    {
+      id: 15,
+      name: "Google Docs with Next.js",
+      coverURL: "/",
+      description:
+        "Next Google Docs is a web app which uses draft.js to create a document for you. It also uses Firebase to store all the user's data.",
+      githubURL: "https://github.com/j471n/next-google-docs",
+      previewURL: "https://google-next-docs.vercel.app/",
+      tools: ["Next.js", "Tailwind CSS", "Firebase", "Draft.js", "Next Auth"],
+      pinned: true,
+    },
+  
+    /* ..............Other Projects......... */
+  ];
